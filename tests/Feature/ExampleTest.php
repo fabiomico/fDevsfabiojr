f98a56a92794af2f542f0e1f56248321632ed818
fabiomico <fabiojrmicohinojosa@gmail.com>
2024-10-24 22:44:41 -0400

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -17,7 +17,7 @@ class ExampleTest extends TestCase
     public function test_the_application_returns_a_successful_response()
     {
         $user = User::factory()->create([
-            'username' => 'lupitadevo', 
+            'username' => 'jairodev', 
         ]);
     
         $this->actingAs($user); 
